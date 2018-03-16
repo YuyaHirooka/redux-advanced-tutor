54f25007236511ed93e04f659ef3aa37de13d383
tis305197 <password>
2018-03-16 16:07:47 +0900

    feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {thunkMiddleware} from 'redux-thunk'
+import {createLogger} from 'redux-logger'
+import {createStore,applyMiddleware} from 'redux'
+import rootReducer from './reducer/reducers'
+import {fetchPosts} from "./action/action";
+
+
+const loggerMiddleware =createLogger
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware,
+    )
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPosts('reactjs'))
+    .then(()=>console.log(store.getState))
 
 ReactDOM.render(
   <App />,
